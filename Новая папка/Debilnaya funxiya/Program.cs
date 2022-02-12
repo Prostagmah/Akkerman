13841f1fab4fce1a0fe0f53e9881b9ead555dd13
Артур <Suppermen1012@gmail.com>
2022-02-12 09:41:26 +0300

    akkerman

---
+++ Новая папка/Debilnaya funxiya/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debilnaya_funxiya
+{
+    class Program
+    {
+        static int A(int n, int m)
+        {
+            if (n == 0)
+                return m + 1;
+            else
+              if ((n != 0) && (m == 0))
+                return A(n - 1, 1);
+            else
+                return A(n - 1, A(n, m - 1));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(A(2, 2));
+            Console.ReadKey();
+        }
+    }
+}
